f5076ff35421e25f2ba0c311d0b2d08f167bd620
Patrik Chytil <patrikch@centrum.cz>
2017-12-04 22:35:27 +0100

    work on models in apps: visit, project

--- project/models.py
+++ project/models.py
@@ -2,72 +2,65 @@ from enum import IntEnum
 
 from django.db import models
 from django.db.models import Q
+from django.utils.translation import ugettext_lazy as _l
+
+from model_utils import Choices
 
 from utils.models.mixins import ActiveFieldMixin, SimpleEnumClassMixin
 
 
 class Region(models.Model, SimpleEnumClassMixin):
 
-    name = models.CharField(blank=False, null=False, max_length=20)
-    slug = models.SlugField(blank=False, null=False, max_length=5)
+    name = models.CharField(verbose_name=_l('name'), blank=False, null=False, max_length=20)
+    slug = models.SlugField(verbose_name=_l('slug'), blank=False, null=False, max_length=5)
 
 
 class District(models.Model, SimpleEnumClassMixin):
 
-    name = models.CharField(blank=False, null=False, max_length=20)
-    region = models.ForeignKey(Region, blank=False, null=False, related_name='districts')
-    slug = models.SlugField(blank=False, null=False, max_length=10)
+    name = models.CharField(verbose_name=_l('name'), blank=False, null=False, max_length=20)
+    region = models.ForeignKey(Region, verbose_name=_l('region'), blank=False, null=False, related_name='districts')
+    slug = models.SlugField(verbose_name=_l('slug'), blank=False, null=False, max_length=10)
 
 
 class Client(models.Model, SimpleEnumClassMixin):
 
-    name = models.CharField(blank=False, null=False, max_length=50)
-    description = models.TextField(blank=True, null=True)
-    slug = models.SlugField(blank=False, null=False, max_length=20)
-
-    @property
-    def active_projects(self):
-        return self.projects.filter(active__isnull=True)
-
-
-class ClientCategory(models.Model):
-
-    name = models.CharField(blank=False, null=False, max_length=50)
-    client = models.ForeignKey(Client, blank=False, null=False, related_name='categories')
-    active = models.BooleanField(blank=False, null=False, default=True)
-    sub_category = models.CharField(blank=False, null=False, max_length=20, default='unknown')
+    name = models.CharField(verbose_name=_l('name'), blank=False, null=False, max_length=50)
+    description = models.TextField(verbose_name=_l('description'), blank=True, null=True)
+    slug = models.SlugField(verbose_name=_l('slug'), blank=False, null=False, max_length=20)
 
 
 class Project(ActiveFieldMixin, SimpleEnumClassMixin):
 
-    name = models.CharField(blank=False, null=False, max_length=50)
-    number = models.PositiveIntegerField(blank=False, null=False)
-    client = models.ForeignKey(Client, blank=False, null=False, related_name='projects')
-    date_from = models.DateField(blank=False, null=False)
-    date_to = models.DateField(blank=True, null=True)
-    test_cluster = models.PositiveIntegerField(blank=True, null=True)
-
-    @property
-    def not_closed_waves(self):
-        return self.waves.filter(Q(status=Wave.PREPARING) | Q(status=Wave.ACTIVE) | Q(status=Wave.FINISHED))
+    name = models.CharField(verbose_name=_l('name'), blank=False, null=False, max_length=50)
+    number = models.PositiveIntegerField(verbose_name=_l('project number'), blank=False, null=False)
+    client = models.ForeignKey(Client, verbose_name=_l('client'), blank=False, null=False, related_name='projects')
+    date_from = models.DateField(verbose_name=_l('project start date'), blank=False, null=False)
+    date_to = models.DateField(verbose_name=_l('project end date'), blank=True, null=True)
+    test_cluster = models.PositiveIntegerField(verbose_name=_l('project test cluster'), blank=True, null=True)
+    limit_for_test = models.PositiveIntegerField(verbose_name=_l('project limit for passing test'), blank=True,
+                                                 null=True)
 
 
 class Wave(models.Model):
 
-    class Status(IntEnum):
-        PREPARING = 1   #can see admin, interviewer not yet
-        ACTIVE = 2      #interviewer can see it
-        FINISHED = 3    #lock for interviewer changes
-        CLOSED = 4      #move to archive
+    STATUS = Choices(
+        (1, 'PREPARING', _l('Preparing')),  # can see admin, interviewer not yet
+        (2, 'ACTIVE', _l('Active')),        # interviewer can see it
+        (3, 'FINISHED', _l('Finished')),    # lock for interviewer changes
+        (4, 'CLOSED', _l('Closed')),        # move to archive
+    )
 
-    number = models.PositiveIntegerField(blank=False, null=False)
-    project = models.ForeignKey(Project, blank=False, null=False, related_name='waves')
-    date_from = models.DateField(blank=False, null=False)
-    date_to = models.DateField(blank=False, null=False)
-    status = models.PositiveIntegerField(choices=Status, default=Status.PREPARING)
-    fee_full = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    fee_part = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    max_delay_for_full_fee = models.PositiveIntegerField(blank=True, null=True, default=0)
+    number = models.PositiveIntegerField(verbose_name=_l('wave number'), blank=False, null=False)
+    project = models.ForeignKey(Project, verbose_name=_l('project'), blank=False, null=False, related_name='waves')
+    date_from = models.DateField(verbose_name=_l('wave start date'), blank=False, null=False)
+    date_to = models.DateField(verbose_name=_l('wave end date'), blank=False, null=False)
+    status = models.PositiveIntegerField(verbose_name=_l('status'), choices=STATUS, default=STATUS.PREPARING)
+    fee_full = models.DecimalField(verbose_name=_l('wave full fee'), max_digits=10, decimal_places=2, blank=True,
+                                   null=True)
+    fee_part = models.DecimalField(verbose_name=_l('wave part fee'), max_digits=10, decimal_places=2, blank=True,
+                                   null=True)
+    max_delay_for_full_fee = models.PositiveIntegerField(verbose_name=_l('max delay for full fee'), blank=True,
+                                                         null=True, default=0)
 
     def __str__(self):
         return 'Project: {}, wave: {}'.format(self.project.name, str(self.number))
@@ -75,14 +68,15 @@ class Wave(models.Model):
 
 class Shop(ActiveFieldMixin):
 
-    name = models.CharField(blank=False, null=False, max_length=50)
-    client_code = models.CharField(blank=False, null=False, max_length=10, default='')
-    client = models.ForeignKey(Client, blank=False, null=False, related_name='shops')
-    district = models.ForeignKey(District, blank=False, null=False, related_name='shops')
-    category = models.ForeignKey(ClientCategory, blank=True, null=True, related_name='shops')
-    city = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    zip = models.CharField(max_length=5)
+    name = models.CharField(verbose_name=_l('name'), blank=False, null=False, max_length=50)
+    client_shop_id = models.CharField(verbose_name=_l('client shop identifier'), blank=False, null=False,
+                                      max_length=10, default='')
+    client = models.ForeignKey(Client, verbose_name=_l('client'), blank=False, null=False, related_name='shops')
+    district = models.ForeignKey(District, verbose_name=_l('district'), blank=False, null=False, related_name='shops')
+    category = models.CharField(verbose_name=_l('category'), blank=True, null=True, max_length=50)
+    city = models.CharField(verbose_name=_l('city'), blank=True, null=True, max_length=100)
+    address = models.CharField(verbose_name=_l('address'), blank=True, null=True, max_length=100)
+    zip = models.PositiveSmallIntegerField(verbose_name=_l('zip code'), blank=True, null=True, max_length=5)
 
     def __str__(self):
         return '{}-{}-{}-{}-{}'.format(self.client.name, self.name, self.type, self.city, self.address)
--- visit/models.py
+++ visit/models.py
@@ -1,44 +1,35 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _l
+
 from project.models import Project, Wave
 from account.models import Interviewer
 from project.models import Shop
 
 
-class Training(models.Model):
-    project = models.ForeignKey(Project)
-    interviewer = models.ForeignKey(Interviewer)
-    score = models.SmallIntegerField()
-    passed = models.DateTimeField(default=timezone.now)
+class Test(models.Model):
+
+    project = models.ForeignKey(Project, verbose_name=_l('project'), blank=False, null=False, related_name='tests')
+    interviewer = models.ForeignKey(Interviewer, verbose_name=_l('interviewer'), blank=False, null=False,
+                                    related_name='tests')
+    score = models.PositiveSmallIntegerField(verbose_name=_l('test score'), blank=False, null=False)
+    passed = models.DateTimeField(verbose_name=_l('test was passed when'), blank=True, null=True)
 
     def __str__(self):
-        return "Training({0},{1}),score={2}".format(str(self.interviewer),
-                                                    str(self.project),
-                                                    self.score)
-
-
-class Bonus(models.Model):
-    wave = models.ForeignKey(Wave)
-    interviewer = models.ForeignKey(Interviewer)
-    zobdobi = models.DateField()
-    bonus = models.DecimalField(decimal_places=2, max_digits=7)
-    description = models.TextField()
-    paid = models.DateField(blank=True)
-
-    def __str__(self):
-        return "Bonus({0},{1}),bonus={2}".format(str(self.interviewer),
-                                                 str(self.wave),
-                                                 self.bonus)
+        return "Test({0},{1}),score={2}".format(str(self.interviewer), str(self.project), str(self.score))
 
 
 class Attachment(models.Model):
-    wave = models.ForeignKey(Wave)
-    shop = models.ForeignKey(Shop)
-    interviewer = models.ForeignKey(Interviewer)
-    recorded = models.DateTimeField(default=timezone.now)
-    data = models.FileField()
-    file_name = models.CharField(max_length=100)
-    type = models.CharField(max_length=10)
+
+    wave = models.ForeignKey(Wave, verbose_name=_l('wave'), null=False, blank=False, related_name='attachments')
+    shop = models.ForeignKey(Shop, verbose_name=_l('shop'), null=False, blank=False, related_name='attachments')
+    interviewer = models.ForeignKey(Interviewer, verbose_name=_l('interviewer'), null=False, blank=False,
+                                    related_name='attachments')
+    recorded = models.DateTimeField(verbose_name=_l('recorded when'), null=False, blank=False, default=timezone.now,
+                                    editable=False)
+    data = models.FileField(verbose_name=_l('data file'), null=False, blank=False, upload_to='data')
+    file_name = models.CharField(verbose_name=_l('name of data file'), max_length=100, null=False, blank=False)
+    type = models.CharField(verbose_name=_l('type of data file'), max_length=10, null=False, blank=False)
 
     def __str__(self):
         return "Attachment({0},{1},{2}),file={3}".format(str(self.interviewer),
@@ -48,35 +39,55 @@ class Attachment(models.Model):
 
 
 class Wage(models.Model):
-    wave = models.ForeignKey(Wave)
-    interviewer = models.ForeignKey(Interviewer)
-    zobdobi = models.DateField()
-    wage = models.DecimalField(decimal_places=2, max_digits=7)
-    description = models.TextField()
-    paid = models.DateField(blank=True)
+
+    interviewer = models.ForeignKey(Interviewer, verbose_name=_l('interviewer'), null=False, blank=False,
+                                    related_name='wages')
+    billing_period = models.DateField(verbose_name=_l('billing period'), null=False, blank=False)
+    amount = models.DecimalField(verbose_name=_l('amount of money'), decimal_places=2, max_digits=7, null=False,
+                                 blank=False)
+    description = models.TextField(verbose_name=_l('description'), blank=True, null=True)
+    requested = models.DateTimeField(verbose_name=_l('description'), blank=False, null=False, default=timezone.now,
+                                     editable=False)
+    paid = models.BooleanField(verbose_name=_l('wage was paid'), blank=False, null=False, default=False)
 
     def __str__(self):
-        return "Wage({0},{1}),wage={2}".format(str(self.interviewer),
-                                               str(self.wave),
-                                               self.wage)
+        return "Wage({0},{1}),wage={2}".format(str(self.interviewer), str(self.billing_period), str(self.amount))
+
+
+class Bonus(models.Model):
+
+    wave = models.ForeignKey(Wave, verbose_name=_l('wave'), null=False, blank=False, related_name='bonuses')
+    interviewer = models.ForeignKey(Interviewer, verbose_name=_l('interviewer'), null=False, blank=False,
+                                    related_name='bonuses')
+    wage = models.ForeignKey(Wage, verbose_name=_l('wage'), null=True, blank=True, related_name='bonuses')
+    amount = models.DecimalField(verbose_name=_l('amount of money'), decimal_places=2, max_digits=7, null=False,
+                                 blank=False)
+    description = models.TextField(verbose_name=_l('description'), null=False, blank=False)
+
+    def __str__(self):
+        return "Bonus({0},{1}),bonus={2}".format(str(self.interviewer),
+                                                 str(self.wave),
+                                                 self.amount)
 
 
 class Visit(models.Model):
-    wave = models.ForeignKey(Wave)
-    shop = models.ForeignKey(Shop)
-    interviewer = models.ForeignKey(Interviewer, null=True)
-    group = models.PositiveIntegerField()
-    date_from = models.DateField()
-    date_to = models.DateField()
-    visit_day = models.DateField(blank=True, null=True)
-    is_pm = models.NullBooleanField(blank=True)
-    data_ident = models.TextField(blank=True, null=True)
-    data_recorded = models.DateTimeField(blank=True, null=True)
-    wage = models.ForeignKey(Wage, blank=True, null=True, related_name='visits')
+
+    wave = models.ForeignKey(Wave, verbose_name=_l('wave'), null=False, blank=False, related_name='visits')
+    shop = models.ForeignKey(Shop, verbose_name=_l('shop'), null=False, blank=False, related_name='visits')
+    interviewer = models.ForeignKey(Interviewer, verbose_name=_l('interviewer'), null=True, blank=True,
+                                    related_name='visits')
+    group = models.PositiveIntegerField(verbose_name=_l('group of shops'), null=False, blank=False)
+    date_from = models.DateField(verbose_name=_l('visit from date'), null=False, blank=False)
+    date_to = models.DateField(verbose_name=_l('visit to date'), null=False, blank=False)
+    visit_day = models.DateField(verbose_name=_l('visit date'), blank=True, null=True)
+    is_pm = models.NullBooleanField(verbose_name=_l('is pm time'), blank=True, null=True)
+    # data = JSONField(verbose_name=_l('questionnaire data'), blank=True, null=True)  # TODO: dodelat
+    data_recorded = models.DateTimeField(verbose_name=_l('data recorded when'), blank=True, null=True)
+    wage = models.ForeignKey(Wage, verbose_name=_l('wage'), blank=True, null=True, related_name='visits')
 
     def __str__(self):
         return "Visit({0},{1},{2}),visit={3},done={4}".format(str(self.interviewer),
                                                               str(self.wave),
                                                               str(self.shop),
-                                                              (str(self.visit_day) if self.visit_day else "-"), \
+                                                              (str(self.visit_day) if self.visit_day else "-"),
                                                               ("y" if self.data_recorded else "n"))
